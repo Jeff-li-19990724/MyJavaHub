044e8a3e2f131091262e09b0ccec3997d2b3c3b1
12442 <12442@DESKTOP-0I867QN>
2020-07-31 09:42:24 +0800

    V1.4

--- Test0731/src/com/neusoft/test1/Test.java
+++ Test0731/src/com/neusoft/test1/Test.java
@@ -4,5 +4,8 @@ public class Test {
 	public static void main(String[] args) {
 		int a=100;
 		System.out.println("hello word"+a);
+		System.out.println("hello word"+a);
+		System.out.println("hello word"+a);
+		System.out.println("hello word"+a);
 	}
 }
